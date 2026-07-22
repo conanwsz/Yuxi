53d5a7b4f19c104d78a15a0f9af4e0b71edfad56
mors <307526966@qq.com>
2026-07-22 11:33:58 +0800

    域名白名单

--- web/vite.config.js
+++ web/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     server: {
+      allowedHosts: [/\.cn-np\.com$/],
       proxy: {
         '^/api': {
           target: env.VITE_API_URL || 'http://api:5050',
