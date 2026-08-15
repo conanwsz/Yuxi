ec6954c7a3af3e88be87cf6fbd43a9061e163ad2
mors <307526966@qq.com>
2026-08-15 11:19:35 +0800

    chore(deploy): 更新生产环境部署脚本
    - 删除旧的 .env.prod 文件
    - 添加 --env-file 参数到 docker compose 命令
    - 确保环境变量文件正确加载到生产环境部署中

--- cn_deploy_prod.sh
+++ cn_deploy_prod.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+rm -rf .env.prod
+
 cp -f .env.cn_prod .env.prod
 
-docker compose -f docker-compose.cn-prod.yml down
+docker compose -f docker-compose.cn-prod.yml --env-file .env.prod down
 
 sleep 1s
 
-docker compose -f docker-compose.cn-prod.yml up -d --build
+docker compose -f docker-compose.cn-prod.yml --env-file .env.prod up -d --build
